c2abb08c11b1c6838f7abada4f03be3431b83e83
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-11-22 11:04:18 -0400

    refactor: remove unecessary file

--- src/data/Filters.data.ts
+++
@@ -1,7 +0,0 @@
-import { BlackListType } from '../types/BlackList.type'
-
-export const blackList: BlackListType[] = [
-  { id: 'a', description: 'GeekHunter' },
-  { id: 'b', description: 'Crossover' },
-  { id: 'c', description: 'Netvagas' },
-]
