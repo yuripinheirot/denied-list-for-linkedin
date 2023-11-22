e7e7dfc5c9d7c7dd4a8d8a9c7ba3d5acc18920b8
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-11-22 18:07:45 -0400

    refactor: change params on send message

--- public/background.js
+++ public/background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (
-    changeInfo.url &&
-    changeInfo.url.includes('https://www.linkedin.com/jobs/search')
+    tab.url.includes('https://www.linkedin.com/jobs/search') &&
+    tab.status === 'complete'
   ) {
     chrome.tabs.sendMessage(tabId, {
       message: 'executeFilter',
