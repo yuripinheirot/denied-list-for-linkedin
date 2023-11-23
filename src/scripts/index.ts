26207363571935ef35a4b098f422af04ecbff9d5
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-11-23 13:39:46 -0400

    feat: implement active unactive extension on script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,18 @@
+import { AppConfigType } from '../types/AppConfig.type'
+import { KeysStorage } from '../types/KeysStorage.type'
 import { executeFilter } from './executeFilter.script'
 
+const getAppConfig = () => {
+  const appConfig = localStorage.getItem(KeysStorage.APP_CONFIG)
+  if (appConfig) {
+    return JSON.parse(appConfig) as AppConfigType
+  }
+
+  return null
+}
 chrome.runtime.onMessage.addListener((request: { message: string }) => {
   // listen for messages sent from background.js
-  if (request.message === 'executeFilter') {
+  if (request.message === 'executeFilter' && getAppConfig()?.active) {
     console.log('Linkedin Filter extension running...')
     executeFilter()
   }
